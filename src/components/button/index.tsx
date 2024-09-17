01fe2f43ae1de4bdeb859279869ff74171eb9e7b
zeroput <zachary_paris@outlook.com>
2024-09-17 18:41:13 +0800

    update button and republish the li. update readme

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,9 +1,45 @@
 
 
-function Button({ title, onClick, variant, color, size = "medium" }: ButtonProps) {
+function Button({ title, onClick, variant, color, size = "medium", sizeNum }: ButtonProps) {
+  let style: any = {
+    padding: "5px 10px"
+  }
+  if (variant === "outlined") {
+    style = {
+      ...style,
+      backgroundColor: "black",
+      border: "1px solid white"
+    }
+  }
+  if (variant === "contained") {
+    style = {
+      ...style,
+      backgroundColor: "black",
+      color: "white"
+    }
+  }
+  // add the size prop
+  if (size) {
+    style = {
+      ...style,
+      size
+    }
+  }
+  if (sizeNum === "A1") {
+    style = {
+      ...style,
+      size: "big"
+    }
+  }
+  if (sizeNum === "A2") {
+    style = {
+      ...style,
+      size: "medium"
+    }
+  }
   return (
     <button
-      onClick={onClick}
+      style={style}
     >
       {title}
     </button>
@@ -15,8 +51,9 @@ export default Button;
 
 export interface ButtonProps {
   title: string;
-  onClick: () => void;
+  onClick?: () => void;
   variant: "contained" | "outlined";
   color?: "red" | "black";
   size?: "small" | "medium" | "large";
+  sizeNum?: "A1" | "A2" | "A3"
 }
